ad2fc661d065f11e3e255d36bbffd9a6da3cca63
KeXiaohao <2239830393@qq.com>
2024-11-04 03:25:27 +0800

    add checkUpdates in MainParser

--- app/src/main/java/com/hustcaster/app/data/MainParser.kt
+++ app/src/main/java/com/hustcaster/app/data/MainParser.kt
@@ -1,6 +1,8 @@
 package com.hustcaster.app.data
 
 import android.util.Log
+import android.util.Xml
+import com.hustcaster.app.network.fetchRssData
 import org.xmlpull.v1.XmlPullParser
 import org.xmlpull.v1.XmlPullParserFactory
 import java.io.StringReader
@@ -16,11 +18,12 @@ object MainParser {
     private lateinit var state: FeedAndFeedItems
     private val factory = XmlPullParserFactory.newInstance()
 
-    fun parse(xmlData: String, rssUrl: String) {
+    fun parse(rssUrl: String) {
         try {
             state = FeedAndFeedItems(Feed(rssUrl))
             factory.isNamespaceAware = true
             val parser = factory.newPullParser()
+            val xmlData = fetchRssData(rssUrl)
             parser.setInput(StringReader(xmlData))
             var eventType = parser.eventType
             var currentItem: FeedItem? = null
@@ -78,7 +81,58 @@ object MainParser {
         }
     }
 
-    fun checkUpdates(){
+    fun checkUpdates(url: String) {
+        try {
+            factory.isNamespaceAware = true
+            val parser = factory.newPullParser()
+            val data = fetchRssData(url)
+            parser.setInput(StringReader(data))
+            var currentItem: FeedItem? = null
+            var eventType = parser.eventType
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                when (eventType) {
+                    XmlPullParser.START_TAG -> {
+                        if (parser.namespace.isNotEmpty()) {
+                            FeedParserFactory.getParser(parser.namespace)?.parse(parser, state)
+                        } else {
+                            val nodeName = parser.name
+                            when (nodeName) {
+                                ITEM -> currentItem = FeedItem(state.feed.id)
+                                TITLE -> if (currentItem == null) {
+                                    state.feed.title = parser.nextText()
+                                } else {
+                                    currentItem.title = parser.nextText()
+                                }
 
+                                LINK -> if (currentItem == null) {
+                                    state.feed.link = parser.nextText()
+                                }
+
+                                DESCRIPTION -> if (currentItem == null) {
+                                    state.feed.description = parser.nextText()
+                                } else {
+                                    currentItem.description = parser.nextText()
+                                }
+
+                                PUB_DATE -> if (currentItem == null) {
+                                    //check updates logic
+                                    state.feed.pubDate = parser.nextText()
+                                } else {
+                                    currentItem.pubDate = parser.nextText()
+                                }
+
+
+                                ENCLOSURE -> currentItem?.audioUrl =
+                                    parser.getAttributeValue(null, "url")
+
+                            }
+                        }
+                    }
+                }
+                eventType = parser.next()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
