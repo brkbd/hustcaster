59e298d0592a75bcba069028a04848d95da0d134
KeXiaohao <2239830393@qq.com>
2024-11-11 05:43:07 +0800

    draw customizedTopAppBar

---
+++ app/src/main/java/com/hustcaster/app/compose/podcast/PodcastInfoView.kt
@@ -0,0 +1,17 @@
+package com.hustcaster.app.compose.podcast
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import java.util.Calendar
+
+@Preview
+@Composable
+fun PodcastInfoView(
+    title: String = "6 minutes English",
+    imageUrl: String = "",
+    author: String = "abc",
+    pubDate: Calendar = Calendar.getInstance(),
+    description: String = "11111111111111111111111111111111"
+) {
+
+}
