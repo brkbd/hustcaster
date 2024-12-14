118616d000f18503f6bdf8b42ab832945f04b7eb
KeXiaohao <2239830393@qq.com>
2024-12-15 04:48:56 +0800

    add update dao

--- app/src/main/java/com/hustcaster/app/data/AppDatabase.kt
+++ app/src/main/java/com/hustcaster/app/data/AppDatabase.kt
@@ -8,17 +8,20 @@ import androidx.room.TypeConverters
 import com.hustcaster.app.data.dao.EpisodeDao
 import com.hustcaster.app.data.dao.PodcastDao
 import com.hustcaster.app.data.dao.RecordDao
+import com.hustcaster.app.data.dao.UpdateDao
 import com.hustcaster.app.data.model.Episode
 import com.hustcaster.app.data.model.Podcast
 import com.hustcaster.app.data.model.Record
+import com.hustcaster.app.data.model.Update
 import com.hustcaster.app.utils.Converters
 
-@Database(version = 1, entities = [Podcast::class, Episode::class, Record::class], exportSchema = false)
+@Database(version = 1, entities = [Podcast::class, Episode::class, Record::class,Update::class], exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun podcastDao(): PodcastDao
     abstract fun episodeDao(): EpisodeDao
     abstract fun recordDao(): RecordDao
+    abstract fun updateDao():UpdateDao
 
     companion object {
         @Volatile
---
+++ app/src/main/java/com/hustcaster/app/data/dao/UpdateDao.kt
@@ -0,0 +1,29 @@
+package com.hustcaster.app.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Transaction
+import com.hustcaster.app.data.model.EpisodeAndUpdate
+import com.hustcaster.app.data.model.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UpdateDao {
+    @Insert
+    suspend fun insertUpdate(update: Update)
+
+    @Delete
+    suspend fun deleteUpdate(update: Update)
+
+    @Query("delete from updates")
+    suspend fun deleteAllUpdates()
+
+    @Query("select * from updates")
+    suspend fun getAllUpdates():Flow<List<Update>>
+
+    @Transaction
+    @Query("select * from updates")
+    suspend fun getAllEpisodeAndUpdate():Flow<List<EpisodeAndUpdate>>
+}
